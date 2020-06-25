8349f54deae05c6d1a8dcd925202a4e8046a16ed
Assoumani <assoumani.mutabaruka@gmail.com>
2020-06-25 10:51:23 +0200

    ajout fichier

---
+++ src/Util/CartPriceCalculator.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Util;
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class CartPriceCalculator
+{
+    public function calculate(ArrayCollection $cart)
+    {
+        $price = 0;
+        foreach ($cart as $sale) {
+            $price += ($sale->getCount() * $sale->getProduct()->getPrice());
+        }
+        return $price;
+    }
+}
---
+++ tests/Controller/ProductControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProductControllerTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', 'fr/product/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertCount(12, $crawler->filter('div.div_product'));
+    }
+}
